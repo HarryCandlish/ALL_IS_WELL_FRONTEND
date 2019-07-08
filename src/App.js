1c429a2c8d9d005e19283077d1c3218e7fd8fc9d
HarryCandlish <hcnd90@gmail.com>
2019-07-08 17:34:17 +1200

    created title and did some design

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React from "react";
+import React, { Component } from "react";
+import { HashRouter as Router, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
 
-import Philosophy from "./components/Philosophy";
 import "./App.css";
+import Home from "./components/Home";
 
 function App() {
   return (
     <Provider store={store}>
-      <div className="App">
-        <Philosophy />
-      </div>
+      <Router>
+        <Route exact path="/" component={Home} />
+      </Router>
     </Provider>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import homeStyles from "../modules/home.module.scss";
+
+import Philosophy from "./Philosophy";
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className={homeStyles.title}>
+          &nbsp;All <br />
+          &nbsp;is
+          <br />
+          &nbsp; Well.
+        </h1>
+        <div className={homeStyles.banner}>
+          <h2 className={homeStyles.secondaryTitle}>The Problem</h2>
+          <p className={homeStyles.epigraph}>
+            An attempt to fight and establish a base in Wellington to deal with
+            the mental health issues in Wellington and New Zealand. We know well
+            enough the issue, it's apart of every individuals life, whether it
+            be a family member, partner, friend. If not you see it in our
+            streets, in the news, in the schools and workplace. It's hard not be
+            encumbered by it, making it easy to ignore for lack of knowing what
+            do.
+          </p>
+        </div>
+        <Philosophy />
+      </div>
+    );
+  }
+}
+
+export default Home;

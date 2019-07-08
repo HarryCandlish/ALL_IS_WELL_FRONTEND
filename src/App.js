081b255cecc3f23461cd015dd32f0037d51aed84
HarryCandlish <hcnd90@gmail.com>
2019-07-09 10:50:08 +1200

    redux setup

--- src/App.js
+++ src/App.js
@@ -1,21 +1,45 @@
 import React, { Component } from "react";
 import { HashRouter as Router, Route } from "react-router-dom";
-import { Provider } from "react-redux";
-import store from "./store";
 
 import "./App.css";
 import Home from "./components/Home";
 import Nav from "./components/Nav";
 
-function App() {
-  return (
-    <Provider store={store}>
-      <Nav />
-      <Router>
-        <Route exact path="/" component={Home} />
-      </Router>
-    </Provider>
-  );
+import { fetchPhilosophy } from "./actions/philosophyAction";
+import { connect } from "react-redux";
+
+export class App extends Component {
+  constructor() {
+    super();
+  }
+  componentDidMount() {
+    this.props.philosophy();
+  }
+  render() {
+    return (
+      <div>
+        <Nav />
+        <Router>
+          <Route exact path="/" component={Home} />
+        </Router>
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    philosophy: state.philosophy
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    philosophy: () => dispatch(fetchPhilosophy())
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);

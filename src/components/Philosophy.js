9dee4af95a5966f033a5351268bd00c28dd7dd5e
HarryCandlish <hcnd90@gmail.com>
2019-07-08 16:13:02 +1200

    redux working, displaying data

--- src/components/Philosophy.js
+++ src/components/Philosophy.js
@@ -1,14 +1,31 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { fetchPhilosophy } from "../actions/philosophyAction";
 
 class Philosophy extends Component {
+  componentWillMount() {
+    this.props.fetchPhilosophy();
+  }
   render() {
+    const philosophyPhilosophy = this.props.philosophy.map(philosophy => (
+      <div key={philosophy.id}>
+        <h1>{philosophy.title}</h1>
+      </div>
+    ));
     return (
       <div>
         <h1>Our Philosophy</h1>
+        {philosophyPhilosophy}
       </div>
     );
   }
 }
 
-export default Philosophy;
+const mapStateToProps = state => ({
+  philosophy: state.philosophy.philosophy
+});
+
+export default connect(
+  mapStateToProps,
+  { fetchPhilosophy }
+)(Philosophy);
